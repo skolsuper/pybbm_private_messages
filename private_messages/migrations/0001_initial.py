615068b057d58919b687a6806d0ed2f418c19f91
James Keys <skolsuper@gmail.com>
2015-08-30 10:03:35 +0700

    Reset migrations.
    Anyone upgrading from <=0.1 will need to dump their private messages into a fixture, run manage.py migrate private_messages zero, then run manage.py migrate again before reimporting the messages.

--- private_messages/migrations/0001_initial.py
+++ private_messages/migrations/0001_initial.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 from django.conf import settings
+import uuid
 
 
 class Migration(migrations.Migration):
@@ -21,20 +22,32 @@ class Migration(migrations.Migration):
             ],
         ),
         migrations.CreateModel(
-            name='PrivateMessage',
+            name='MessageThread',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+            ],
+            options={
+                'ordering': ('-messages',),
+            },
+        ),
+        migrations.CreateModel(
+            name='PrivateMessage',
+            fields=[
                 ('body', models.TextField(verbose_name='Message')),
                 ('body_html', models.TextField(verbose_name='HTML version')),
                 ('body_text', models.TextField(verbose_name='Text version')),
+                ('uuid', models.UUIDField(default=uuid.uuid4, serialize=False, primary_key=True)),
                 ('sender_ip', models.IPAddressField(default='0.0.0.0', verbose_name='Sender IP', blank=True)),
                 ('sent', models.DateTimeField(auto_now_add=True, verbose_name='Sent', db_index=True)),
                 ('subject', models.CharField(default='[No Subject]', max_length=100, blank=True)),
                 ('sender_deleted', models.BooleanField(default=False)),
-                ('receivers', models.ManyToManyField(related_name='inbox', verbose_name='Recipients', through='private_messages.MessageHandler', to=settings.AUTH_USER_MODEL)),
-                ('sender', models.ForeignKey(related_name='outbox', verbose_name='Sender', to=settings.AUTH_USER_MODEL)),
+                ('receivers', models.ManyToManyField(related_name='recd_messages', verbose_name='Recipients', through='private_messages.MessageHandler', to=settings.AUTH_USER_MODEL)),
+                ('sender', models.ForeignKey(related_name='sent_messages', verbose_name='Sender', to=settings.AUTH_USER_MODEL)),
+                ('thread', models.ForeignKey(related_name='messages', to='private_messages.MessageThread')),
             ],
             options={
+                'ordering': ['sent'],
+                'get_latest_by': 'sent',
                 'verbose_name': 'Private Message',
                 'verbose_name_plural': 'Private Messages',
             },
