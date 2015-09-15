1bbc1fab976dd63e6a2f05aa35117dc74db40652
James Keys <skolsuper@gmail.com>
2015-09-15 15:29:34 +0700

    Use ModelSelectField.  Javascript still broken for some reason.

--- private_messages/forms.py
+++ private_messages/forms.py
@@ -2,18 +2,22 @@
 from __future__ import unicode_literals
 
 from django import forms
+from django.contrib.auth import get_user_model
 from django.utils.translation import ugettext_lazy as _
 
-from django_select2.fields import HeavySelect2MultipleChoiceField
+from django_select2.fields import HeavyModelSelect2MultipleChoiceField
 from pybb import util
 
 from private_messages.models import PrivateMessage
+
+User = get_user_model()
 
 
 class MessageForm(forms.ModelForm):
 
     parent = forms.UUIDField(required=False, widget=forms.HiddenInput)
-    receivers = HeavySelect2MultipleChoiceField(data_view='private_messages:receivers_json')
+    receivers = HeavyModelSelect2MultipleChoiceField(
+        data_view='private_messages:receivers_json', queryset=User.objects.all())
 
     class Meta(object):
         model = PrivateMessage
